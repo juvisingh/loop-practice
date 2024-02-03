b3905bd0a0acb9c3a3e346f8c54bc26831d4c9c0
juvisingh <141285456+juvisingh@users.noreply.github.com>
2024-02-02 18:20:53 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+var i = 0;
+//sum of every number from 1 to 1000
+var everySum = 0;
+for (i = 0; i < 1001; i ++) {
+    everySum += i;
+}
+console.log(everySum);
+
+//sum of the first 10 natural numbers
+var naturalSum = 0;
+for (i = 0; i < 11; i ++) {
+    naturalSum += i;
+}
+console.log(naturalSum);
+
+//sum of every odd number from 93 to 845
+var oddNumSum = 0;
+for (i = 93; i <= 845; i += 2) {
+    oddNumSum += i
+}
+console.log(oddNumSum);
+
+//sum of odd numbers from the first 10 natural numbers
+var oddNatSum = 0
+for (i = 1; i < 11; i+= 2) {
+    oddNatSum += i
+}
+console.log(oddNatSum)
+
+
